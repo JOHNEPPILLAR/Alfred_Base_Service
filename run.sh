e94ac176d61a3bfba0cdda33fb24d0a199b10872
JOHNEPPILLAR <johneppillar@gmail.com>
2020-06-27 18:01:19 +0100

    Bump eslinter

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 clear
 
 echo "Removing node modules folder and installing latest"
-#rm -rf node_modules
-#rm package-lock.json
-#ncu -u
+rm -rf node_modules
+rm package-lock.json
+ncu -u
 npm install
 npm audit fix
 snyk test
