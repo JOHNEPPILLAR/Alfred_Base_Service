20a4b2fce5b9714c5d567428fcfaeb87031d2c99
JOHNEPPILLAR <johneppillar@gmail.com>
2020-06-27 18:07:21 +0100

    Updated to not remove node libs

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 clear
 
-echo "Removing node modules folder and installing latest"
-rm -rf node_modules
-rm package-lock.json
+echo "Installing latest"
+#rm -rf node_modules
+#rm package-lock.json
 ncu -u
 npm install
 npm audit fix
