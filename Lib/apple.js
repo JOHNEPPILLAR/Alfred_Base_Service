1484c8af170462fa9fb2d407d8249c0f935fe545
JOHNEPPILLAR <johneppillar@gmail.com>
2020-06-27 14:59:21 +0100

    Added apn and bank holiday checker functions

---
+++ Lib/apple.js
@@ -0,0 +1,106 @@
+/**
+ * Import external libraries
+ */
+const apn = require('apn');
+
+/**
+ * Send IOS push notification
+ *
+ * @param {String} Notification text
+ *
+ */
+async function _sendPushNotification(notificationText) {
+  try {
+    const deviceTokens = [];
+    const pushSQL =
+      'SELECT last(device_token, time) as device_token FROM ios_devices';
+
+    this.logger.debug(
+      `${this._traceStack()} - Connect to data store connection pool`,
+    );
+    const dbConnection = await this._connectToDB('devices');
+
+    this.logger.debug(`${this._traceStack()} - Get IOS devices`);
+    const devicesToNotify = await dbConnection.query(pushSQL);
+
+    this.logger.debug(
+      `${this._traceStack()} - Release the data store connection back to the pool`,
+    );
+    await dbConnection.end(); // Close data store connection
+
+    if (devicesToNotify.rowCount === 0) {
+      this.logger.trace(`${this._traceStack()} - No devices to notify`);
+      return;
+    } // Exit function as no devices to process
+
+    // Send iOS notifications what watering has started
+    this.logger.debug(
+      `${this._traceStack()} - Build list of devices to send push notification to`,
+    );
+    devicesToNotify.rows.map((device) =>
+      deviceTokens.push(device.device_token),
+    );
+
+    // Connect to apples push notification service and send notifications
+    const IOSNotificationKeyID = await this._getVaultSecret.call(
+      this,
+      process.env.ENVIRONMENT,
+      'IOSNotificationKeyID',
+    );
+    const IOSNotificationTeamID = await this._getVaultSecret.call(
+      this,
+      process.env.ENVIRONMENT,
+      'IOSNotificationTeamID',
+    );
+    const IOSPushKey = await this._getVaultSecret.call(
+      this,
+      process.env.ENVIRONMENT,
+      'IOSPushKey',
+    );
+    if (
+      IOSNotificationKeyID instanceof Error ||
+      IOSNotificationTeamID instanceof Error ||
+      IOSPushKey instanceof Error
+    ) {
+      this.logger.error(
+        `${this._traceStack()} - Not able to get secret (CERTS) from vault`,
+      );
+      return;
+    }
+
+    const apnProvider = new apn.Provider({
+      token: {
+        key: IOSPushKey,
+        keyId: IOSNotificationKeyID,
+        teamId: IOSNotificationTeamID,
+      },
+      production: false,
+    });
+
+    this.logger.debug(`${this._traceStack()} - Send push notification(s)`);
+    const notification = new apn.Notification();
+    notification.topic = 'JP.Alfred';
+    notification.expiry = Math.floor(Date.now() / 1000) + 600; // Expires 10 minutes from now.
+    notification.alert = notificationText;
+    const result = await apnProvider.send(notification, deviceTokens);
+
+    if (result.sent.length > 0) {
+      this.logger.info(`Push notification sent: ${notificationText}`);
+    } else {
+      this.logger.error(
+        `${this._traceStack()} - Push notification faild to be sent`,
+      );
+    }
+
+    this.logger.debug(
+      `${this._traceStack()} - Close down connection to push notification service`,
+    );
+    await apnProvider.shutdown(); // Close the connection with apn
+  } catch (err) {
+    this.logger.error(`${this._traceStack()} - ${err.message}`);
+  }
+}
+
+module.exports = {
+  _sendPushNotification,
+};
--- Lib/google.js
+++ Lib/google.js
@@ -4,6 +4,7 @@
 const { google } = require('googleapis');
 const moment = require('moment');
 const geolib = require('geolib');
+const dateFormat = require('dateformat');
 
 /**
  * Get Google calendar
@@ -117,9 +118,52 @@ async function _inJPWorkGeoFence(lat, long) {
   );
 }
 
+/**
+ * Check if it's a weekend or bacnk holiday
+ */
+async function _isBankHolidayWeekend() {
+  this.logger.debug(
+    `${this._traceStack()} - Check for bank holidays and weekends`,
+  );
+  const url = 'https://www.gov.uk/bank-holidays.json';
+  const toDay = new Date();
+  const isWeekend = toDay.getDay() === 6 || toDay.getDay() === 0;
+
+  if (isWeekend) {
+    this.logger.debug(`${this._traceStack()} - It's the weekend`);
+    return true;
+  }
+
+  const returnData = await this._callAPIServiceGet(url);
+  if (returnData instanceof Error) {
+    this.logger.error(`${this._traceStack()} - ${returnData.message}`);
+    return returnData;
+  }
+
+  let bankHolidays = [];
+  try {
+    bankHolidays = returnData['england-and-wales'].events;
+    if (bankHolidays.length === 0) throw Error('No bank holiday data');
+  } catch (err) {
+    this.logger.error(`${this._traceStack()} - ${err.message}`);
+    return err;
+  }
+
+  bankHolidays = bankHolidays.filter(
+    (a) => a.date === dateFormat(toDay, 'yyyy-mm-dd'),
+  );
+  if (bankHolidays.length === 0) {
+    this.logger.debug(`${this._traceStack()} - It's the weekday`);
+    return false;
+  }
+  this.logger.debug(`${this._traceStack()} - It's ${bankHolidays[0].title}`);
+  return true;
+}
+
 module.exports = {
   _getGoogleCal,
   _kidsAtHomeToday,
   _inHomeGeoFence,
   _inJPWorkGeoFence,
+  _isBankHolidayWeekend,
 };
